e0e8782ec4796b553f1515ff01d91d24b321f9d1
Manco <santiagomancomaya@gmail.com>
2026-04-26 16:41:23 -0500

    Change ip

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-export VITE_API_BASE_URL=http://34.61.240.128:3000
-export CORS_ORIGIN=http://34.61.240.128,http://127.0.0.1
+export VITE_API_BASE_URL=http://34.28.203.6:3000
+export CORS_ORIGIN=http://34.28.203.6,http://127.0.0.1
 
 docker compose down
 docker compose build --no-cache
