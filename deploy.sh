cb54b9a8edf920a804285811f36d66c78a5ff6f8
Manco <santiagomancomaya@gmail.com>
2026-04-26 15:43:36 -0500

    Deploy.sh update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-export VITE_API_BASE_URL=http://34.30.103.224:3000
-export CORS_ORIGIN=http://34.30.103.224,http://127.0.0.1
+export VITE_API_BASE_URL=http://34.61.240.128:3000
+export CORS_ORIGIN=http://34.61.240.128,http://127.0.0.1
 
 docker compose up -d --build
